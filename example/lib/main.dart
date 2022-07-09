472e7845848741072fce920739dc026006e53f6b
John <bjohnoluwakeye@gmail.com>
2022-07-09 16:34:49 +0100

    cleanups

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:lumen/lumen.dart';
 import 'package:flutter/material.dart';
 import 'package:lumen_flutter_example/app.dart';
 
-const lumenApiKey = "0chNiXNhssz4sVovTitulBkFI65Iaz23No";
+const lumenApiKey = "<< your-api-key >>";
 
 void main() {
   Lumen.init(lumenApiKey);
